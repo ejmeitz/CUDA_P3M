676a376f3fac8c5443c1aa24f995b09dec64a256
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-11-24 13:42:18 -0500

    code done, unchecked

--- python/P3M.py
+++ python/P3M.py
@@ -7,6 +7,8 @@ import math
 
 from .helper import *
 
+#similar code: https://github.com/jht0664/structurefactor_spme/blob/master/run_sq.py
+
 #Calculate E_direct
 def particle_particle(r, q, alpha, r_cut, real_lat):
     N_atoms = len(q)
@@ -57,29 +59,50 @@ def M(u, n):
 def dMdu(u,n):
     return M(u, n-1) - M(u-1, n-1)
 
-#Make this non-allocating
-def calc_b(m1,m2,m3,K1,K2,K3,n): #n is spline order
-    b1 = np.exp(2*np.pi*1.0j*(n-1)*m1/K1) / sum([M(k+1, n)*np.exp(2*np.pi*1.0j*m1*k/K1) for k in range(n-1)])
-    b2 = np.exp(2*np.pi*1.0j*(n-1)*m2/K2) / sum([M(k+1, n)*np.exp(2*np.pi*1.0j*m2*k/K2) for k in range(n-1)])
-    b3 = np.exp(2*np.pi*1.0j*(n-1)*m3/K3) / sum([M(k+1, n)*np.exp(2*np.pi*1.0j*m3*k/K3) for k in range(n-1)])
-    return b1, b2, b3
+# Make this non-allocating
+# Make this not use np.exp(j) just manage cos() and sin() manually
+def b(m, K, n): #n is spline order
+    return np.exp(2*np.pi*1.0j*(n-1)*m/K) / sum([M(k+1, n)*np.exp(2*np.pi*1.0j*m*k/K) for k in range(n-1)])
 
 def calc_B(m1,m2,m3,K1,K2,K3,n):
-    b1,b2,b3 = calc_b(m1,m2,m3,K1,K2,K3,n)
-    return (b1**2)*(b2**2)*(b3**2) #notation in papers makes no sense, absolute squared??
+    b1 = b(m1,K1,n)
+    b2 = b(m2,K2,n)
+    b3 = b(m3,K3,n)
+    return (b1.real**2 + b1.imag**2)*(b2.real**2 + b2.imag**2)*(b3.real**2 + b3.imag**2)
 
-def calc_C(alpha, V, m1, m2, m3):
+
+def calc_C(alpha, V, m1, m2, m3, K1, K2, K3):
     if (m1 == 0 and m2 == 0 and m3 == 0):
         return 0
     else:
-        m_norm = np.norm([m1,m2,m3]) #is this what the paper means?
-        return (1/(np.pi*V))*(np.exp(-(np.pi**2)*m_norm/(alpha**2))/m_norm)
+        if m1 <= 0 or m1 <= K1/2:
+            m1 -= K1
+        elif m2 <= 0 or m2 <= K2/2:
+            m2 -= K2
+        elif m3 <= 0 or m3 <= K3/2:
+            m3 -= K3
+        m_sq = m1*m1 + m2*m2 + m3*m3 #is this what the paper means?
+        return (1/(np.pi*V))*(np.exp(-(np.pi**2)*m_sq/(alpha**2))/m_sq)
+    
+
+def calc_BC(alpha, V, K1, K2, K3, n):
+    BC = np.zeros((K1,K2,K3))
+
+    for m1 in range(K1):
+        for m2 in range(K2):
+            for m3 in range(K3):
+                BC = calc_B(m1, m2, m3, K1, K2, K3, n) * calc_C(alpha, V, m1, m2, m3, K1, K2, K3)
+
+    return BC
+
 
 #Calculate E_reciprocal
-def particle_mesh(r, q, real_lat, recip_lat, alpha, spline_interp_order, r_cut_recip, mesh_dims):
+def particle_mesh(r, q, real_lat, alpha, spline_interp_order, k_max, mesh_dims):
 
     N_atoms = len(q)
-    N1, N2, N3 = get_N_cells(recip_lat, r_cut_recip)
+
+    recip_lat = reciprocal_vecs(real_lat)
+    
     K1, K2, K3 = mesh_dims
 
     V = vol(real_lat)
@@ -91,41 +114,67 @@ def particle_mesh(r, q, real_lat, recip_lat, alpha, spline_interp_order, r_cut_r
 
     #Fill Q array (interpolate charge onto grid)
     Q = np.zeros((K1, K2, K3))
-    B = np.zeros((K1, K2, K3))
-    C = np.zeros((K1, K2, K3))
+    dQdr = np.zeros((N_atoms, 3, K1, K2, K3)) #deriv in real space
+    BC = calc_BC(alpha, V, K1, K2, K3, spline_interp_order)
 
-    for k1 in range(K1): #loop over points on mesh
+    for k1 in range(K1):
         for k2 in range(K2):
             for k3 in range(K3):
 
-                #Build up B,C array (are these in recip space?? already)
-                m1 = k1; m2 = k2; m3 = k3
-                B[m1, m2, m3] = calc_B(m1,m2,m3,K1,K2,K3,spline_interp_order)
-                C[m1, m2, m3] = calc_C(alpha, V, m1, m2, m3)
+                 
+                #* not sure this loop is right
+                #& my current interpretation is that for a given grid point (k1,k2,k3) get contribution from all other grid pts
+                for p1 in range(K1):
+                    for p2 in range(K2):
+                        for p3 in range(K3):                            
+                            for i in range(N_atoms):
+                                u0 = u[i,0] - k1 - p1*K1; u1 = u[i,1]- k2 - p2*K2; u2 = u[i,2] - k3 - p3*K3
 
-                for i in range(N_atoms): #loop over periodic images in recip space
-                    for n1 in range(N1):
-                        for n2 in range(N2):
-                            for n3 in range(N3):
+                                I = dMdu(u0) * M(u1) * M(u2)
+                                II = M(u0) * dMdu(u1) * M(u2)
+                                III = M(u0) * M(u1) * dMdu(u2)
+
+                                #dQdr_i0 #& not sure indexing is right on recip_lat
+                                dQdr[i, 0, k1, k2, k3] += (K1 * recip_lat[0,0] * I) + (K2 * recip_lat[1,0] * II) + (K3 * recip_lat[2,0]  * III)
+                                #dQdr_i1
+                                dQdr[i, 1, k1, k2, k3] += (K1 * recip_lat[0,1] * I) + (K2 * recip_lat[1,1] * II) + (K3 * recip_lat[2,1]  * III)
+                                #dQdr_i2
+                                dQdr[i, 2, k1, k2, k3] += (K1 * recip_lat[0,2] * I) + (K2 * recip_lat[1,2] * II) + (K3 * recip_lat[2,2]  * III)
+
                                 #Real space charge interpolated onto mesh
-                                Q[k1,k2,k3] += q[i] * M(u[i,0] - k1- n1*K1) * \
-                                                      M(u[i,1]- k2 - n2*K2) * \
-                                                      M(u[i,2] - k3 - n3*K3)
+                                Q[k1,k2,k3] += q[i] * M(u0) * M(u1) * M(u2)
+                                
+
     
     #Invert Q (do in place on GPU??)
     Q_recip = np.fft.fftn(Q)
 
-    theta_rec = B*C #element-wise? #are these already in fourier space??
+    E_out = np.zeros((K1,K2,K3)) #how to get this per atom??
 
-    conv_res = sig.convolve(theta_rec, Q_recip)
-
-    E_out = 0.0
     for m1 in range(K1):
         for m2 in range(K2):
             for m3 in range(K3):
-                E_out += conv_res[m1,m2,m3] * Q_recip[m1,m2,m3]
+                if m1 == 0 and m2 == 0 and m3 == 0:
+                    continue
 
-    return E_out/2
+                E_out[m1,m2,m3] += Q_recip[m1,m2,m2]*Q_recip[-m1,-m2,-m3]*BC[m1,m2,m3]
+
+                #Update values in Q array to make conv (theta_rec, Q) easier
+                Q_recip[m1,m2,m3] *= BC[m1,m2,m3]
+
+
+    E_out /= (2*np.pi*V)
+
+    #Q_recipt has B and C multipled into it now, so IFFT of this is conv(Theta_rec, Q)
+    theta_rec = np.fft.ifft(Q)
+
+    F_out = np.zeros(N_atoms, 3)
+    for i in range(N_atoms):
+        for dir in range(3):
+            F_out[i, dir] = -np.sum(dQdr[i,dir,:,:,:] * theta_rec) #do this without allocating
+ 
+
+    return E_out, F_out
 
 
 
@@ -134,16 +183,19 @@ def self_energy(q, alpha):
 
 
 
-def PME(r, q, error_tol, r_cut, spline_interp_order):
+def PME(r, q, real_lat_vec, error_tol, r_cut_real, spline_interp_order):
+
+
+    L = [np.norm(real_lat_vec[i,:]) for i in range(real_lat_vec.shape[0])]
 
     # OpenMM parameterizes like this:
     # http://docs.openmm.org/latest/userguide/theory/02_standard_forces.html#coulomb-interaction-with-particle-mesh-ewald
-    alpha = np.sqrt(-np.log(2*error_tol))/r_cut
+    alpha = np.sqrt(-np.log(2*error_tol))/r_cut_real
     #Look at FFT implementation it can be better if this is a factor of some prime number
-    n_mesh = np.ceil(2*alpha*d/(3*np.power(error_tol, 0.2))) #assumes cubic box
+    n_mesh = np.ceil(2*alpha*L/(3*np.power(error_tol, 0.2))) #assumes cubic box
 
-    pp_energy, pp_force = particle_particle(r, q, alpha)
-    pm_energy, pm_force = particle_mesh(r, q, alpha, n_mesh, spline_interp_order)
+    pp_energy, pp_force = particle_particle(r, q, alpha, r_cut_real, real_lat_vec)
+    pm_energy, pm_force = particle_mesh(r, q, real_lat_vec, alpha, spline_interp_order, n_mesh)
     self_eng = self_energy(r, q)
 
     U_SPME = pp_energy + pm_energy + self_eng
@@ -173,9 +225,8 @@ if __name__ == "__main__":
 
     r_cut_real = 4.0 #idk this is random
     r_cut_neighbor = r_cut_real + 1.0 # also not sure what this should be
-    r_cut_recip = None #how tf u choose this?
     error_tol = 1e-5 #on GPU OpenMM warns this is lower limit and error can start going up (should check when we do GPU)
-    spline_interp_order = 4
+    spline_interp_order = 5
 
     dump_path = r"c"
 
