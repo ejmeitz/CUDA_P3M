b22aa7e40ae12931357d60eadd9eb4b726e4fb01
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-12-14 13:01:59 -0500

    fix BC

--- LongRangeInteractions/src/methods/SPME/splines.jl
+++ LongRangeInteractions/src/methods/SPME/splines.jl
@@ -18,23 +18,24 @@ function dMdu(u,n)
     return M(u, n-1) - M(u-1, n-1)
 end
 
-#Remove allocation? probably not a huge deal this is precalculated
 function b(mⱼ,n,Kⱼ)
-    m_K = mⱼ/Kⱼ
-    num = exp(2*π*1im*(n-1)*m_K)
-    denom = sum([M(k+1, n)*exp(2*π*1im*k*m_K) for k in 0:n-2])
+    m_K = 2*π*mⱼ/Kⱼ
+    v = m_K*(n-1)
+    num = cos(v) + 1im*sin(v)
+    denom = 0.0 + 0.0im
+    for k in 0:n-2
+        v2 = m_K*k
+        denom += M(k+1, n)*(cos(v2) + 1im*sin(v2))
+    end
     return  num/denom 
 end
 
 function calc_C(β, V, ms, recip_lat)
-    m_star = ms[1]*recip_lat[1,:] + ms[2]*recip_lat[2,:] + ms[3]*recip_lat[3,:]
+    m_star = ms[1].*recip_lat[1] .+ ms[2].*recip_lat[2] .+ ms[3].*recip_lat[3]
     m_sq = dot(m_star,m_star)
     return (1/(π*V))*(exp(-(π^2)*m_sq/(β^2))/m_sq)
 end
     
-function abs2(x::Complex)
-    return (real(x)^2) + (imag(x)^2)
-end
 
 function calc_BC(spme::SPME)
     V = vol(spme.sys)
@@ -43,28 +44,36 @@ function calc_BC(spme::SPME)
     n = spline_order(spme)
 
     BC = zeros(ComplexF64, K1, K2, K3)
-    hs = [0.0, 0.0, 0.0]
+    hs = [0,0,0]
 
-    for m1 in 0:K1-1
-        hs[1] = (m1 <= (K1/2) ? m1 : m1 - K1)
-        B1 = abs2(b(m1,K1,n))
-        for m2 in 0:K2-1
-            hs[2] = ((m2 <= (K2/2)) ? m2 : m2 - K2)
-            B2 = B1*abs2(b(m2,K2,n))
-            for m3 in 0:K3-1
-                hs[3] = ((m3 <= (K3/2)) ? m3 : m3 - K3)
+    #Gather h indices 
+    h1s = collect(0:(K1-1))
+    h1s[h1s .> K1/2] .-= K1 #* this has allocation
 
-                if m1 == 0 && m2 == 0 && m3 == 0
-                    continue
-                end
-                
-                B3 = B2*abs2(b(m3,K3,n))
+    h2s = collect(0:(K2-1))
+    h2s[h2s .> K2/2] .-= K2
+
+    h3s = collect(0:(K3-1))
+    h3s[h3s .> K3/2] .-= K3
+
+    B1 = abs2.(b.(0:K1-1, n, K1))
+    B2 = abs2.(b.(0:K2-1, n, K2))
+    B3 = abs2.(b.(0:K3-1, n, K3))
+
+    hs = [0,0,0]
+    for m1 in range(0,K1-1)
+        hs[1] = h1s[m1+1]
+        for m2 in range(0,K2-1)
+            hs[2] = h2s[m2+1]
+            for m3 in range(0,K3-1) 
+                hs[3] = h3s[m3+1]        
                 C = calc_C(spme.β, V, hs, recip_lat)
-
-                BC[m1,m2,m3] = B3*C
+                BC[m1+1,m2+1,m3+1] = B1[m1+1]*B2[m2+1]*B3[m3+1]*C
             end
         end
     end
+
+    BC[1,1,1] = 0
 
     return BC
 end
